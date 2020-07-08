8e85fbd79a1ebf85b129bf80b9bb8e13760a6626
Destiny-16 <destiny6@mail.usf.edu>
2020-07-08 19:48:52 -0400

    Update GUI.java
    start and stop buttons are now clickable

--- GUI.java
+++ GUI.java
@@ -1,20 +1,44 @@
 import javax.swing.*;
 import java.awt.*;
 import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 
 public class GUI extends JFrame {
+
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    Board board = new Board();
+    JButton start = new JButton("Start");
+    JButton stop = new JButton("Stop");
+
+    JLabel count = new JLabel("0");
+    JLabel countLabel = new JLabel("Count:");
+
 
     int spacing = 1;
 
     public GUI(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        Board board = new Board();
-        JButton start = new JButton("Start");
-        JButton stop = new JButton("Stop");
 
-        JLabel count = new JLabel("0");
-        JLabel countLabel = new JLabel("Count:");
+       start.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               System.out.println("starts program");
+
+           }
+       });
+
+       stop.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("stops program");
+
+        }
+    });
 
         panel.setLayout(new GridLayout(0,1));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
@@ -32,7 +56,9 @@ public class GUI extends JFrame {
         frame.setTitle("Langton's Ant");
 
 
-      
+        
+        
+
     }
 
     public class Board extends JPanel {
@@ -49,4 +75,6 @@ public class GUI extends JFrame {
 
         }
     }
-}
+
+}  
+
